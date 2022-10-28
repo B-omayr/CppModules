5558bc74124e213616c3638ba4182009c12ff2b5
omayrib <iomayr@student.1337.ma>
2022-10-28 20:19:15 +0100

    finished Moduel-06

--- Module-06/ex02/main.cpp
+++ Module-06/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iomayr <iomayr@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ibra <ibra@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:18:08 by iomayr            #+#    #+#             */
-/*   Updated: 2022/10/26 18:43:04 by iomayr           ###   ########.fr       */
+/*   Updated: 2022/10/28 20:17:53 by ibra             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,82 @@
 #include "B.hpp"
 #include "C.hpp"
 
-Base *generate(void)
+Base * generate(void)
 {
+    srand(time(NULL));
     
+    int r = rand() % 3;
+    
+    if (r == 0)
+    {
+        std::cout << "A" << std::endl;
+        return new A();
+    }
+    else if (r == 1)
+    {
+        std::cout << "B" << std::endl;
+        return new B();
+    }
+    else
+    {
+        std::cout << "C" << std::endl;
+        return new C();
+    }
 }
 
-int main()
-{
+void identify(Base* p){
+    A *a;
+    B *b;
+    C *c;
     
+    if ((a = dynamic_cast<A*>(p)))
+        std::cout << "1 - We Got Type A" << std::endl;
+    else if ((b = dynamic_cast<B*>(p)))
+        std::cout << "1 - We Got Type B" << std::endl;
+    else if ((c = dynamic_cast<C*>(p)))
+        std::cout << "1 - We Got Type C" << std::endl;
+}
+
+void identify(Base& p){    
+    try
+    {
+        A &a = dynamic_cast<A&>(p);
+        (void)a;
+        std::cout << "2 - We Got Type A" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        (void)e;
+    }
+    try
+    {
+        B &b = dynamic_cast<B&>(p);
+        (void)b;
+        std::cout << "2 - We Got Type B" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        (void)e;
+    }
+    try
+    {
+        C &c = dynamic_cast<C&>(p);
+        (void)c;
+        std::cout << "2 - We Got Type C" << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        (void)e;
+    }
+
+} 
+
+int main(){
+
+    Base *b;
+    
+    b = generate();
+    b = generate();
+    identify(b); 
+    identify(*b); 
 }
