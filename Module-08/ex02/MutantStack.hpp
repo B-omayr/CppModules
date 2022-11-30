abc6b51bcbd24ac814d3aa27b9490e75e0d686c6
omayrib <iomayr@student.1337.ma>
2022-11-30 18:55:53 +0100

    start ex02

---
+++ Module-08/ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iomayr <iomayr@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 16:58:11 by iomayr            #+#    #+#             */
+/*   Updated: 2022/11/30 17:35:06 by iomayr           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+    public:
+        
+};
+
+#endif
