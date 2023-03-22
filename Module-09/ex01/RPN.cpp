58b023d46e1e0fa8389295547e391436b6047676
omayrib <iomayr@student.1337.ma>
2023-03-22 11:04:04 +0000

    add Orthodox canonical Form to ex01

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,14 +6,96 @@
 /*   By: ibra <ibra@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:39:08 by ibra              #+#    #+#             */
-/*   Updated: 2023/03/20 18:42:17 by ibra             ###   ########.fr       */
+/*   Updated: 2023/03/22 10:59:00 by ibra             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RPN_HPP
-# define RPN_HPP
+#include "RPN.hpp"
 
-#include <iostream>
-#include <stack>
+Rpn::Rpn(){
+    //Xx
+}
 
-#endif
+Rpn::Rpn(const Rpn &copy){
+    *this = copy;
+}
+
+Rpn &Rpn::operator = (const Rpn &Input)
+{
+    if (this != &Input){
+        this->myStack = Input.myStack;
+    }
+    return *this;
+}
+
+Rpn::~Rpn(){
+    //Xx
+}
+
+void checkValidChar(char c)
+{
+    if (!isdigit(c) && c != '*' && c != '+' && c != '-' && c != '/')
+        throw std::runtime_error("Error: Invalid character");
+}
+
+int division(int num1, int num2)
+{
+    int val;
+    
+    if (num2 == 0)
+        throw std::runtime_error("Error: Division by 0");
+    val = num1 / num2;
+    return val;
+}
+
+void Rpn::operation(char oper)
+{
+    int num1;
+    int num2;
+    int val;
+
+    if (myStack.size() >= 2)
+    {
+        num2 = myStack.top();
+        myStack.pop();
+        num1 = myStack.top();
+        myStack.pop();
+        if (oper == '+')
+            val = num1 + num2;
+        else if (oper == '-')
+            val = num1 - num2;
+        else if (oper == '*')
+            val = num1 * num2;
+        else
+            val = division(num1, num2);
+    }
+    myStack.push(val);
+}
+
+void Rpn::exec(std::string buffer)
+{
+    for (int i = 0; buffer[i]; i++)
+    {
+        if(buffer[i] != ' '){
+            checkValidChar(buffer[i]);
+            if (buffer[i] == '+'){
+                operation('+');
+            }
+            else if (buffer[i] == '-'){
+                operation('-');
+            }
+            else if (buffer[i] == '*'){
+                operation('*');                
+            }
+            else if (buffer[i] == '/'){
+                operation('/');
+            }
+            else{
+                myStack.push(buffer[i] - 48);
+            }
+        }
+    }
+    if (myStack.size() > 1)
+        throw std::runtime_error("Error: Size of Stack");
+    std::cout << myStack.top() << std::endl;
+}
--- Module-09/ex01/RPN.hpp
+++ Module-09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: ibra <ibra@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 18:42:09 by ibra              #+#    #+#             */
-/*   Updated: 2023/03/20 18:42:27 by ibra             ###   ########.fr       */
+/*   Updated: 2023/03/22 10:59:24 by ibra             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@
 
 #include <iostream>
 #include <stack>
+
+class Rpn{
+    
+    private:
+        std::stack<int> myStack;
+    public:
+        Rpn();
+        Rpn(const Rpn &copy);
+        Rpn &operator = (const Rpn &Input);
+        ~Rpn();
+
+        void exec(std::string buffer);
+        void operation(char oper);
+              
+};
 
 #endif
